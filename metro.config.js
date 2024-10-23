61b302006a70905adf14f8ff71ea61bf9b707208
Lubni Morais <lubni.morais@gmail.com>
2024-10-23 11:01:24 -0300

    bug: error after install rn-mdx

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,26 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-module.exports = {
-  resolver: {
-    sourceExts: ['js', 'ts', 'tsx', 'jsx', 'json', 'md', 'mdx', 'ttf'],
-  },
-}
+// module.exports = {
+//   resolver: {
+//     sourceExts: ['js', 'ts', 'tsx', 'jsx', 'json', 'md', 'mdx', 'ttf'],
+//   },
+// }
+
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg", 'js', 'ts', 'tsx', 'jsx', 'json', 'md', 'mdx']
+  };
+
+  return config;
+})();
